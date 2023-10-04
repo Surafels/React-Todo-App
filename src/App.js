22e0ec0e9f19189258ac64a63f71ad8af2433499
Surafels <ssurafel4@gmail.com>
2023-10-04 17:12:34 +0300

    Add header and render it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <TodoApp />
     </div>
   );
 }
---
+++ src/components/TodosLogic.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TodosLogic = () => (
+  <div>
+    TodosLogic
+  </div>
+);
+
+export default TodosLogic;
