1df444c58e39ac7ed598f485f19a1e0cdaef8a29
Surafels <ssurafel4@gmail.com>
2023-10-04 19:36:06 +0300

    Add functionality for submit list and delete

---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TodoItem = ({ itemProp, handleChange, delTodo }) => (
+  <li>
+    <input
+      type="checkbox"
+      checked={itemProp.completed}
+      onChange={() => handleChange(itemProp.id)}
+    />
+    <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+    {itemProp.title}
+  </li>
+);
+
+export default TodoItem;
