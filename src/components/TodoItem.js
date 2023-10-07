7c33d8d2c97bd36aeb21b10df6d78fda5fe32d48
Surafels <ssurafel4@gmail.com>
2023-10-07 12:24:45 +0300

    Add edit button functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => (
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+    const [editing, setEditing] = useState(false);
+    const handleEditing = () => {
+        setEditing(true);
+      };
+      let viewMode = {};
+      let editMode = {};
+      if (editing) {
+        viewMode.display = 'none';
+      } else {
+        editMode.display = 'none';
+      }
+      return (
   <li>
     <input
       type="checkbox"
       checked={itemProp.completed}
       onChange={() => handleChange(itemProp.id)}
     />
+   
+    
+        <button onClick={handleEditing}>Edit</button>
+        <input  type="text"
+      value={itemProp.title}
+      className="textInput"
+        />
+
     <button onClick={() => delTodo(itemProp.id)}>Delete</button>
     {itemProp.title}
+
   </li>
-);
+      )
+};
 
 export default TodoItem;
